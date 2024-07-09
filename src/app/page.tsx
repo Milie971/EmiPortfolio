ba74651245833f7524be0413fb625e59e2bf01e8
Milie971 <e.lolia971@gmail.com>
2024-07-10 00:12:55 +0200

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,14 @@ import { Footer } from "@/components/Footer";
 export default function Home() {
   return (
     <main>
+      <Image
+        src="/nextjs-github-pages/vercel.svg"
+        alt="Vercel Logo"
+        className={styles.vercelLogo}
+        width={100}
+        height={24}
+        priority
+      />
       <Header />
 
       <Spacing size="md" />
